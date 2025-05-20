e9e1bb04b1843ed487324b1890d5bc8b916b6599
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2025-05-20 20:33:32 +0000

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :rsvps
-  resources :follow_requests
-  resources :comments
-  resources :events
-  devise_for :users
+  
   root "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :comments
+  resources :events
+  resources :follow_requests
+  resources :rsvps
+  
+  
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
