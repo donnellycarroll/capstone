d883e48995e31b5afcccfad1e6672e38f4a9c2aa
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2025-05-20 21:06:07 +0000

    fixed order in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  
+
   devise_for :users
 
   resources :comments
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :rsvps
   
-  
+  get "/users/:id" => "users#show", as: :user
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
